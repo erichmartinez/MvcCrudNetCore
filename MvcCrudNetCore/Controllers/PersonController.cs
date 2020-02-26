cef3e1018a9561d6307034d3952cfb129e961729
Eric Martinez <eric.hiram.martinez@outlook.com>
2020-02-25 21:11:08 -0600

    Initial Commit

---
+++ MvcCrudNetCore/Controllers/PersonController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MvcCrudNetCore.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace MvcCrudNetCore.Controllers
+{
+    public class PersonController : Controller
+    {
+        private IConfiguration _configuration;
+
+        public PersonController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET: Person
+        public ActionResult Index()
+        {
+            PersonDBHandler PersonHandler = new PersonDBHandler(_configuration);
+            return View(PersonHandler.GetPersonList());
+        }
+
+        // GET: Person/Details/5
+        public ActionResult Details(int id)
+        {
+            PersonDBHandler PersonHandler = new PersonDBHandler(_configuration);
+            return View(PersonHandler.GetPersonList().Find(personmodel => personmodel.ID == id));
+        }
+
+        // GET: Person/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Person/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(PersonModel iList)
+        {
+            try
+            {
+                PersonDBHandler PersonHandler = new PersonDBHandler(_configuration);
+                if (PersonHandler.InsertPerson(iList))
+                {
+                    ModelState.Clear();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Person/Edit/5
+        public ActionResult Edit(int id)
+        {
+            PersonDBHandler PersonHandler = new PersonDBHandler(_configuration);
+            return View(PersonHandler.GetPersonList().Find(personmodel => personmodel.ID == id));
+        }
+
+        // POST: Person/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, PersonModel iList)
+        {
+            try
+            {
+                PersonDBHandler PersonHandler = new PersonDBHandler(_configuration);
+                PersonHandler.UpdatePerson(iList);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Person/Delete/5
+        public ActionResult Delete(int id)
+        {
+            PersonDBHandler PersonHandler = new PersonDBHandler(_configuration);
+            return View(PersonHandler.GetPersonList().Find(personmodel => personmodel.ID == id));
+        }
+
+        // POST: Person/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                PersonDBHandler PersonHandler = new PersonDBHandler(_configuration);
+                if (PersonHandler.DeletePerson(id))
+                {
+                    ModelState.Clear();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MvcCrudNetCore/Models/PersonDBHandler.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace MvcCrudNetCore.Models
+{
+    public class PersonDBHandler
+    {
+        private SqlConnection con;
+        private IConfiguration _configuration;
+        public PersonDBHandler(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private void connection()
+        {
+            string constring = _configuration.GetConnectionString("PeopleDBConn");
+            con = new SqlConnection(constring);
+        }
+        public bool InsertPerson(PersonModel iList)
+        {
+            connection();
+            string query = "INSERT INTO Person VALUES('" + iList.Name + "','" + iList.Address + "','" + iList.Phone + "','" + iList.Email + "')";
+            SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+
+        public List<PersonModel> GetPersonList()
+        {
+            connection();
+            List<PersonModel> iList = new List<PersonModel>();
+
+            string query = "SELECT * FROM Person";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            con.Open();
+            adapter.Fill(dt);
+            con.Close();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                iList.Add(new PersonModel
+                {
+                    ID = Convert.ToInt32(dr["ID"]),
+                    Name = Convert.ToString(dr["Name"]),
+                    Address = Convert.ToString(dr["Address"]),
+                    Phone = Convert.ToString(dr["Phone"]),
+                    Email = Convert.ToString(dr["Email"])
+                });
+            }
+            return iList;
+        }
+
+        public bool UpdatePerson(PersonModel iList)
+        {
+            connection();
+            string query = "UPDATE Person SET Name = '" + iList.Name + "', Address = '" + iList.Address + "', Phone = '" + iList.Phone + "', Email = '" + iList.Email + "' WHERE ID = " + iList.ID;
+            SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+
+        public bool DeletePerson(int id)
+        {
+            connection();
+            string query = "DELETE FROM Person WHERE ID = " + id;
+            SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+    }
+}
